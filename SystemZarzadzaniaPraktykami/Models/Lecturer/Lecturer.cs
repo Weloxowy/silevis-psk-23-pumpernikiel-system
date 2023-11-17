c423e907ce5ca7fcc98c5d2813075fd801214bce
Paweł <karpawell@gmail.com>
2023-11-17 21:16:38 +0100

    Lecturer

---
+++ SystemZarzadzaniaPraktykami/Models/Lecturer/Lecturer.cs
@@ -0,0 +1,25 @@
+﻿using FluentNHibernate.Utils;
+
+namespace SystemZarzadzaniaPraktykami.Models.Lecturer
+{
+    public class Lecturer
+    {
+        public Lecturer() : base() { }
+
+        public Lecturer(Guid id, string Name, string Surname, string Login, string Password)
+        {
+            this.id = id;
+            this.Name = Name;
+            this.Surname = Surname;
+            this.Login = Login;
+            this.Password = Password;
+
+        }
+        public virtual Guid id { get; set; }
+        public virtual string Name { get; set; }
+        public virtual string Surname { get; set; }
+        public virtual string Login { get; set; }   
+        public virtual string Password { get; set; }
+
+    }
+}
---
+++ SystemZarzadzaniaPraktykami/Models/Lecturer/LecturerMapping.cs
@@ -0,0 +1,18 @@
+﻿using FluentNHibernate.Mapping;
+
+namespace SystemZarzadzaniaPraktykami.Models.Lecturer
+{
+    public class LecturerMapping : ClassMap<Lecturer>
+    {
+        readonly string tablename = nameof(Lecturer);
+        public LecturerMapping()
+        {
+            Id(x => x.id).GeneratedBy.Guid();
+            Map(x => x.Name);
+            Map(x => x.Surname);
+            Map(x => x.Login);
+            Map(x => x.Password);
+
+        }
+    }
+}
