60b293fde6ba0a64d4885e1c0d3c0fc6923d5539
Jakeb65 <kuba.kielce100@gmail.com>
2023-11-18 04:18:03 +0100

    dodanie mainPage dla studenta z przyciskiem do zmiany języków

---
+++ SystemZarzadzaniaPraktykami/Pages/MainPageStudent/mainPage.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SystemZarzadzaniaPraktykami.Pages.MainPage
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
